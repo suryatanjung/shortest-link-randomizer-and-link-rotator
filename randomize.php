08212efc5a1a7fde23858ec363fbfa7b50405173
jung <tan@gmx.sg>
2024-02-02 23:53:06 +0800

    add link rotator

--- randomize.php
+++ randomize.php
@@ -5,3 +5,5 @@ header("Location:" . array_rand(["link1", "link2", "link3"]));
 // Using rand
 header("Location:" . ['link1', 'link2', 'link3'][rand(0, 2)]);
 
+// Link Rotator
+$u = ['/link1', '/link2', '/link3']; $f = './count.txt'; file_put_contents($f, ++$i = +file_get_contents($f)) % 3 && header("Location:$u[$i]");
