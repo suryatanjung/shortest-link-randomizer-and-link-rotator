ee6a2095b9262e4900347c3e1f99550122762df9
jung <tan@gmx.sg>
2024-02-02 23:32:16 +0800

    add alternative

--- randomize.php
+++ randomize.php
@@ -1,1 +1,7 @@
-<?php header("Location:" . array_rand(["link1", "link2", "link3"]))^die;
+<?php 
+// Using array_rand
+header("Location:" . array_rand(["link1", "link2", "link3"]))^die;
+
+// Using rand
+header("Location:" . ['link1', 'link2', 'link3'][rand(0, 2)])&die;
+
