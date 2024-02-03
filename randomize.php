09e592b394d0781782b6499780157b956d51586b
jung <tan@gmx.sg>
2024-02-03 15:44:06 +0800

    delete bracket

--- randomize.php
+++ randomize.php
@@ -10,4 +10,4 @@ $u = ['link1', 'link2', 'link3'];
 $f = './count.txt';
 $i = file_exists($f) ? (file_get_contents($f) + 1) % count($u) : 0;
 file_put_contents($f, $i);
-header("Location: {$u[$i]}");
+header("Location: $u[$i]");
