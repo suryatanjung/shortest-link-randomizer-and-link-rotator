0df423a7b0d2b95e399d87ed1da273c1570c10d3
jung <tan@gmx.sg>
2024-02-03 16:18:46 +0800

    add my name

--- randomize.php
+++ randomize.php
@@ -11,3 +11,5 @@ $f = './count.txt';
 $i = ((file_exists($f) ? file_get_contents($f) : -1) + 1) % count($u);
 file_put_contents($f, $i);
 header("Location:$u[$i]");
+
+// code by github.com/suryatanjung
