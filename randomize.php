023765f445d311e5d85ab82df802468112909fc7
jung <tan@gmx.sg>
2024-02-05 15:09:35 +0800

    separate rotator

--- randomize.php
+++ randomize.php
@@ -1,12 +1,3 @@
 <?php 
-// Link Randomizer
+// Link Randomizer by github.com/suryatanjung
 header("Location:" . ['link1', 'link2', 'link3'][rand(0, 2)]);
-
-// Link Rotator
-$u = ['https://google.com', 'https://facebook.com', 'https://twitter.com'];
-$f = './count.txt';
-$i = ((file_exists($f) ? file_get_contents($f) : -1) + 1) % count($u);
-file_put_contents($f, $i);
-header("Location:$u[$i]");
-
-// Code by github.com/suryatanjung
