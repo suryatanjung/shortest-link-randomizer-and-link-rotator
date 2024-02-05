f33b685c75ce5e4c1334c864b44f15b84558c818
jung <tan@gmx.sg>
2024-02-05 15:11:42 +0800

    Create rotator.php

---
+++ rotator.php
@@ -0,0 +1,7 @@
+<?php
+// Link Rotator by github.com/suryatanjung
+$u = ['link1', 'link2', 'link3'];
+$f = './count.txt';
+$i = ((file_exists($f) ? file_get_contents($f) : -1) + 1) % count($u);
+file_put_contents($f, $i);
+header("Location:$u[$i]");
